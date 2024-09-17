ccacd5d62c9ab4f9dadcb707eddbae5aa04054dc
CristianBaqueroB <cfbaquero@unicesar.edu.co>
2024-09-17 13:00:14 -0500

    modificaciones

--- lib/packages/interesSimple/capitalIS.dart
+++ lib/packages/interesSimple/capitalIS.dart
@@ -4,44 +4,42 @@ class CapitalPage extends StatefulWidget {
   const CapitalPage({super.key});
 
   @override
+ 
   _CapitalPageState createState() => _CapitalPageState();
 }
 
 class _CapitalPageState extends State<CapitalPage> {
   final _futureAmountController = TextEditingController();
   final _interestRateController = TextEditingController();
-  final _timeController = TextEditingController();
-  String _timeUnit = "Años"; // Valor por defecto: Años
+  final _yearsController = TextEditingController();
+  final _monthsController = TextEditingController();
+  final _daysController = TextEditingController();
   String _interestRateType = "Anual"; // Valor por defecto: Anual
   double? _calculatedCapital;
 
   void _calculateCapital() {
     final futureAmount = double.tryParse(_futureAmountController.text);
     final interestRate = double.tryParse(_interestRateController.text);
-    final time = double.tryParse(_timeController.text);
+    final years = double.tryParse(_yearsController.text) ?? 0;
+    final months = double.tryParse(_monthsController.text) ?? 0;
+    final days = double.tryParse(_daysController.text) ?? 0;
 
-    if (futureAmount != null && interestRate != null && time != null && interestRate != 0 && time != 0) {
-      double timeInYears;
+    if (futureAmount != null && interestRate != null && interestRate != 0) {
+      // Convertir tiempo a años dependiendo de los valores ingresados
+      final timeInYears = years;
+      final timeInYearsMonths = months / 12;
+      final timeInYearsDays = days / 365;
 
-      // Convertir tiempo a años dependiendo de la unidad seleccionada
-      if (_timeUnit == "Meses") {
-        timeInYears = time / 12;
-      } else if (_timeUnit == "Días") {
-        timeInYears = time / 365;
+      // Ajustar la tasa de interés si es mensual
+      double adjustedInterestRate;
+      if (_interestRateType == "Mensual") {
+        adjustedInterestRate = (interestRate / 100)/12;
       } else {
-        timeInYears = time;
+        adjustedInterestRate = interestRate/100; // Tasa anual
       }
 
-      // Ajustar la tasa de interés si es anual y el tiempo está en meses
-      double adjustedInterestRate  ;
-      if (_interestRateType == "Mensual" ) {
-        adjustedInterestRate = interestRate/ 12;
-      }else  {
-        adjustedInterestRate = interestRate; // Tasa anual
-      }
-
-      // Calcular capital usando la fórmula: Capital = Monto Futuro / (1 + Tasa * Tiempo)
-      final capital = futureAmount / (1+(adjustedInterestRate) * timeInYears);
+      // Calcular capital usando la fórmula: Capital = Monto Futuro / (1 + Tasa * (años + meses/12 + días/365))
+      final capital = futureAmount / ( (adjustedInterestRate) * (timeInYears + timeInYearsMonths + timeInYearsDays));
       setState(() {
         _calculatedCapital = capital;
       });
@@ -52,8 +50,9 @@ class _CapitalPageState extends State<CapitalPage> {
     setState(() {
       _futureAmountController.clear();
       _interestRateController.clear();
-      _timeController.clear();
-      _timeUnit = "Años"; // Restablecer la unidad por defecto
+      _yearsController.clear();
+      _monthsController.clear();
+      _daysController.clear();
       _interestRateType = "Anual"; // Restablecer el tipo de tasa por defecto
       _calculatedCapital = null; // Limpiar el resultado
     });
@@ -70,17 +69,16 @@ class _CapitalPageState extends State<CapitalPage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-           
             SizedBox(height: 20),
             TextField(
               controller: _futureAmountController,
               keyboardType: TextInputType.number,
               decoration: InputDecoration(
-                labelText: 'Monto Futuro',
+                labelText: 'Interes',
                 border: OutlineInputBorder(),
               ),
             ),
-            SizedBox(height: 10),
+            SizedBox(height: 15),
             TextField(
               controller: _interestRateController,
               keyboardType: TextInputType.number,
@@ -90,40 +88,58 @@ class _CapitalPageState extends State<CapitalPage> {
               ),
             ),
             SizedBox(height: 10),
-            TextField(
-              controller: _timeController,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: 'Tiempo',
-                border: OutlineInputBorder(),
+            // Card para ingresar años, meses y días
+            Card(
+              elevation: 6,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Tiempo:",
+                      style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: TextField(
+                            controller: _yearsController,
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                              labelText: 'Años',
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 5),
+                        Expanded(
+                          child: TextField(
+                            controller: _monthsController,
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                              labelText: 'Meses',
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 5),
+                        Expanded(
+                          child: TextField(
+                            controller: _daysController,
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                              labelText: 'Días',
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-            SizedBox(height: 10),
-            Row(
-              children: [
-                Text(
-                  "Unidad de Tiempo: ",
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontFamily: 'Roboto',
-                  ),
-                ),
-                SizedBox(width: 10),
-                DropdownButton<String>(
-                  value: _timeUnit,
-                  items: <String>["Años", "Meses", "Días"].map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                  onChanged: (String? newValue) {
-                    setState(() {
-                      _timeUnit = newValue!;
-                    });
-                  },
-                ),
-              ],
             ),
             SizedBox(height: 10),
             Row(
@@ -158,7 +174,6 @@ class _CapitalPageState extends State<CapitalPage> {
               children: [
                 ElevatedButton(
                   onPressed: _calculateCapital,
-                  child: Text("Calcular"),
                   style: ElevatedButton.styleFrom(
                     foregroundColor: Colors.white,
                     backgroundColor: Colors.blue,
@@ -168,12 +183,13 @@ class _CapitalPageState extends State<CapitalPage> {
                       fontFamily: 'Roboto',
                     ),
                   ),
+                  child: Text("Calcular"),
                 ),
                 ElevatedButton(
                   onPressed: _clearFields,
                   style: ElevatedButton.styleFrom(
                     foregroundColor: Colors.white,
-                    backgroundColor: Colors.red, // Cambia el color del botón de limpiar
+                    backgroundColor: Colors.red,
                     padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 24.0),
                     textStyle: TextStyle(
                       fontSize: 18,
